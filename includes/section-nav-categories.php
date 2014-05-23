2cd4006c2caa1242b42038e4880b4ef8ab7c7b7b
Brad Frost <frostbd@gmail.com>
2014-05-23 18:10:43 -0400

    Category section nav structure

--- includes/section-nav-categories.php
+++ includes/section-nav-categories.php
@@ -1,6 +1,11 @@
 <nav class="vertical-nav section-nav">
 	<h3 class="section-nav-title">Categories</h3>
 	<ul class="section-nav-list">
-		<?php wp_list_categories(); ?>
+		<?php 
+			$args = array(
+				'title_li'           => "",
+			);
+			wp_list_categories($args); 
+		?>
 	</ul>
 </nav><!--end .section-nav-->
