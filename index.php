47c446754bdd9d9293132c774f48b8ae213e48f8
Brad Frost <frostbd@gmail.com>
2014-05-23 17:57:52 -0400

    Update blog markup/structure

--- index.php
+++ index.php
@@ -6,15 +6,19 @@
 			<h1 class="page-title"><?php echo get_the_title(23); ?></h1>
 			<p class="page-intro"><?php the_field('tagline', 23); ?></p>
 		</div>
+		<ul class="post-list">
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<div class="block block-post">
-			<div class="b-body">
-				<p class="b-timestamp"><a href="<?php echo get_permalink(); ?>"></a></p>
-				<h3 class="b-title"><a href="<?php echo get_permalink(); ?>"> <?php echo get_the_title(); ?> </a></h3>
-				<p class="b-excerpt"><?php echo get_the_excerpt(); ?></p>
+		<li>
+			<div class="block block-post">
+				<div class="b-body">
+					<p class="b-timestamp"><a href="<?php echo get_permalink(); ?>"><?php echo get_the_date(); ?></a></p>
+					<h3 class="b-title"><a href="<?php echo get_permalink(); ?>"> <?php echo get_the_title(); ?> </a></h3>
+					<p class="b-excerpt"><?php echo get_the_excerpt(); ?></p>
+				</div>
 			</div>
-		</div>
+		</li>
 		<?php endwhile; endif; ?>
+		</ul><!--end post-list-->
 		<?php get_template_part('nav', 'below'); ?>
 	</div><!--end .l-main-->
 	<div class="l-sidebar">
