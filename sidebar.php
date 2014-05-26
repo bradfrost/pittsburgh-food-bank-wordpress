071e6df3966f08802de3adbe07c155e1be82cbfe
Brad Frost <frostbd@gmail.com>
2014-05-26 18:29:53 -0400

    Clean up sidebar markup

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,7 @@
 <aside id="sidebar" role="complementary">
 <?php if ( is_active_sidebar('primary-widget-area') ) : ?>
-<div id="primary" class="widget-area">
-<ul class="xoxo">
+<ul class="sidebar-widgets">
 <?php dynamic_sidebar('primary-widget-area'); ?>
 </ul>
-</div>
 <?php endif; ?>
 </aside>
