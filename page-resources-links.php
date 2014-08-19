4bc6c53d4cd83231a2f878b7dc17398219fbbce1
Brad Frost <frostbd@gmail.com>
2014-08-18 22:49:32 -0400

    Added page header image

--- page-resources-links.php
+++ page-resources-links.php
@@ -6,7 +6,7 @@
 
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<div class="page-header">
+<div class="page-header <?php the_field('header_image'); ?>">
 	<h1 class="page-title"><?php the_title(); ?></h1>
 	<p class="page-intro"><?php the_field('tagline'); ?></p>
 </div>
--- page-sitemap.php
+++ page-sitemap.php
@@ -5,8 +5,12 @@
 ?>
 
 <?php get_header(); ?>
-<div class="main-body">
-	<div class="lc text">
+<div class="page-header <?php the_field('header_image'); ?>">
+	<h1 class="page-title"><?php the_title(); ?></h1>
+	<p class="page-intro">A list of all pages on the website</p>
+</div>
+<div class="lc lc-single">
+	<div class="text">
 		<ul>
 		<?php wp_list_pages( $args ); ?>
 		</ul>
