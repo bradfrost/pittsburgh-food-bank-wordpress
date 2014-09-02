2bd08453ee71aadbd8612865dafc5be0bbbdc4fc
Brad Frost <frostbd@gmail.com>
2014-09-02 12:17:19 -0400

    Made newsletter Wordpress-editable

--- includes/donate.php
+++ includes/donate.php
@@ -1,7 +1,7 @@
 <!--Donate-->
 <div class="donate">
 	<div class="lc lc-single">
-		<h2 class="section-title"><?php echo get_field('donate_widget_title',1833); ?></h2>
+		<h2 class="section-title"><?php the_field('donate_widget_title',1833); ?></h2>
 		<?php include (TEMPLATEPATH . '/includes/donate-form.php');  ?>
 	</div><!-- end .lc-->
 </div><!-- end .donate-slider-->
--- includes/well.php
+++ includes/well.php
@@ -10,11 +10,5 @@
 	</div>
 </div><!--end .well-->
 <?php else : ?>
-<div class="well">
-	<div class="lc lc-single">
-		<h2 class="well-title">Join Our Newsletter</h2>
-		<p class="well-desc">Stay up-to-date on upcoming events, program news, etc lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-		<?php include (TEMPLATEPATH . '/includes/newsletter-form.php');  ?>
-	</div>
-</div><!--end .well-->
+	<?php include (TEMPLATEPATH . '/includes/well-newsletter.php');  ?>
 <?php endif; ?>
