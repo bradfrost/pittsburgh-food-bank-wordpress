49aed19624ca80c11a45003982fe48d5215bf256
Brad Frost <frostbd@gmail.com>
2014-09-05 12:56:09 -0400

    New CTA icons, added CTA to Get Help template

--- includes/well-newsletter.php
+++ includes/well-newsletter.php
@@ -1,4 +1,4 @@
-<div class="well">
+<div class="well well-newsletter">
 	<div class="lc lc-single">
 		<h2 class="well-title"><?php the_field('newsletter_widget_title',1832); ?></h2>
 		<p class="well-desc"><?php the_field('newsletter_widget_desc',1832); ?></p>
