9f4be63e3c9312f03038af7d81b5d571c9662b84
Brad Frost <frostbd@gmail.com>
2014-10-11 15:47:42 -0400

    Update newsletter form link

--- includes/newsletter-form.php
+++ includes/newsletter-form.php
@@ -1,4 +1,4 @@
-<a class="btn" href="https://gpcfb.convio.net/site/Survey">Sign Up</a>
+<a class="btn" href="https://secure2.convio.net/gpcfb/site/SSurvey;jsessionid=6FA931F810367FB54F1286E16C0C950A.app271a?ACTION_REQUIRED=URI_ACTION_USER_REQUESTS&SURVEY_ID=1741">Sign Up</a>
 <?php /*
 <form action="https://gpcfb.convio.net/site/Survey" method="post" class="inline-form newsletter-form">           
     <input type="hidden" id="SURVEY_ID" name="SURVEY_ID" value="1903" />
